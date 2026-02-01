3954f6e2912d54107331f8469c94d146373d9f04
Youssef Khalouk <ykhalouk@c4r6p1.1337.ma>
2026-02-01 19:21:19 +0100

    update

--- ex0/main.py
+++ ex0/main.py
@@ -1,18 +1,28 @@
 
+from enum import Enum
 from ex0.CreatureCard import CreatureCard
 
+
+class Players(Enum):
+    DRAGON = "Fire Dragon"
+    GOBLIN = "Goblin Warrior"
+
+
 if __name__ == "__main__":
-    dragon = CreatureCard("Fire Dragon", 5, "Legendary", 7, 5)
+
+    dragon = CreatureCard(Players.DRAGON.value, 5, "Legendary", 7, 5)
     print("CreatureCard Info:")
     print(dragon.get_card_info())
 
     print("\nPlaying Fire Dragon with 6 mana available:")
 
+    game_state = {"mana": 6}
+
     print("Playable:", dragon.is_playable(6))
-    print("Play result:", dragon.play({"mana": 6}))
+    print("Play result:", dragon.play(game_state))
 
     print("\nFire Dragon attacks Goblin Warrior:")
-    goblin = CreatureCard('Goblin Warrior', 5, 'Legendary', 7, 5)
+    goblin = CreatureCard(Players.GOBLIN.value, 5, 'Legendary', 7, 5)
     print(f'Attack result: {dragon.attack_target(goblin)}')
 
     available_mana = 3
